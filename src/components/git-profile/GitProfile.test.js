aa34f5b2d5c0b151c6d5bef0e272658338a93092
KiruthikaJeyashankar <81218987+KiruthikaJeyashankar@users.noreply.github.com>
2024-03-26 20:07:54 +0530

    [add] intial setup for Gitprofile demo
    This commit can be cherry-picked for the demo

--- src/components/git-profile/GitProfile.test.js
+++ src/components/git-profile/GitProfile.test.js
@@ -4,13 +4,13 @@ import userEvent from "@testing-library/user-event"
 
 describe('Git profile test', () => {
     const gitProfileUrl = "/mock/url"
-    it('should render button to load data', () => {
+    test('should render button to load data', () => {
         render(<GitProfile url={gitProfileUrl} />)
 
         expect(screen.getByRole('button', { name: 'Load data' })).toBeInTheDocument()
     })
 
-    it('should show Loader text when Load data button is clicked', () => {
+    test('should show Loader text when Load data button is clicked', () => {
         render(<GitProfile url={gitProfileUrl} />)
 
         userEvent.click(screen.getByRole('button', { name: 'Load data' }))
