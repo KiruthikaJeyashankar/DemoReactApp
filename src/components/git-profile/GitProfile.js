775c23865bef1b3895c2fd2b80cc6d803b6f607e
KiruthikaJeyashankar <81218987+KiruthikaJeyashankar@users.noreply.github.com>
2024-03-28 20:08:17 +0530

    add: gitProfile demo code

--- src/components/git-profile/GitProfile.js
+++ src/components/git-profile/GitProfile.js
@@ -1,16 +1,24 @@
+import axios from 'axios'
 import React, { useState } from 'react'
 
 function GitProfile({ url }) {
     const [isLoading, setIsLoading] = useState(false)
+    const [userName, setUserName] = useState()
 
     function handleLoadData() {
         setIsLoading(true)
+        axios.get(url).then(function (response) {
+            // handle success
+            setUserName(response.data.login)
+        })
+        //logic
     }
 
     return (
         <div>
+            {userName && <div>Hello {userName}!</div>}
             {isLoading && <div>Loading...</div>}
-            <button title='Load data' onClick={handleLoadData}>Load data</button>
+            {!isLoading && <button title='Load data' onClick={handleLoadData}>Load data</button>}
         </div>
     )
 }
--- src/components/git-profile/GitProfile.test.js
+++ src/components/git-profile/GitProfile.test.js
@@ -1,9 +1,22 @@
-import { render, screen } from "@testing-library/react"
+import { render, screen, waitFor } from "@testing-library/react"
 import GitProfile from "./GitProfile"
 import userEvent from "@testing-library/user-event"
+import axios from "axios"
+import { when } from "jest-when"
+
+jest.mock("axios")
 
 describe('Git profile test', () => {
     const gitProfileUrl = "/mock/url"
+    beforeEach(() => {
+        when(axios.get).calledWith(gitProfileUrl).mockResolvedValue({
+            data: {
+                "login": "MockUser1",
+                //..other parts
+            }
+        })
+    })
+
     test('should render button to load data', () => {
         render(<GitProfile url={gitProfileUrl} />)
 
@@ -18,5 +31,30 @@ describe('Git profile test', () => {
         expect(screen.getByText("Loading...")).toBeInTheDocument()
     })
 
-    
+    test('should not show the Load data button when it is loading', () => {
+        render(<GitProfile url={gitProfileUrl} />)
+
+        userEvent.click(screen.getByRole("button", { name: "Load data" }))
+
+        expect(screen.getByText("Loading...")).toBeInTheDocument()
+        expect(screen.queryByRole("button", { name: "Load data" })).not.toBeInTheDocument()
+    })
+
+    test('should make network call when Load data button is clicked', () => {
+        render(<GitProfile url={gitProfileUrl} />)
+
+        userEvent.click(screen.getByRole("button", { name: "Load data" }))
+
+        expect(axios.get).toHaveBeenCalledWith(gitProfileUrl)
+    })
+
+    test('should show the data when Load data button clicked and network call is done', async () => {
+        render(<GitProfile url={gitProfileUrl} />)
+
+        userEvent.click(screen.getByRole("button", { name: "Load data" }))
+
+        await waitFor(() => {
+            expect(screen.getByText("Hello MockUser1!")).toBeInTheDocument()
+        })
+    })
 })
